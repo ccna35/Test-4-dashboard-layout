7e23011084fbdbb8274b14e042a29117047dbf55
ccna35 <shawkykhalil@gmail.com>
2023-01-25 21:09:29 +0200

    "fixed offcanvas"

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 const menuIcon = document.querySelector(".menu-icon");
 const closeIcon = document.querySelector(".close-icon");
-const leftSideBg = document.querySelector(".left-side");
-const leftSide = document.querySelector(".body");
+const leftSide = document.querySelector(".left-side");
 
 menuIcon.addEventListener("click", () => {
   leftSide.classList.add("show");
-  leftSideBg.classList.add("show-bg");
 });
 
 closeIcon.addEventListener("click", () => {
   leftSide.classList.remove("show");
-  leftSideBg.classList.remove("show-bg");
 });
